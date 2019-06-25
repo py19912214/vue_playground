f6e43e3f94abfe21b2ba1a2cb6f2c2ee197329b6
PanYue <yue.pan3@56qq.com>
2019-06-25 18:32:17 +0800

    'cypress'

---
+++ cypress_demo/cypress/integration/demo1.js
@@ -0,0 +1,6 @@
+describe('The Home Page', function() {
+    it('successfully loads', function() {
+      cy.visit('http://localhost:8080') // change URL to match your dev URL
+      cy.get("#testBU").click();
+    })
+  })
